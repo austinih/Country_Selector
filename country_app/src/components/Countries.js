d2d629b50833faf3d4200b69e25b3ef354004736
Austin <austinih46@gmail.com>
2023-02-24 13:34:07 -0600

    rendered country data

--- country_app/src/components/Countries.js
+++ country_app/src/components/Countries.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 export default function Countries () {
-    return (
-        <div>
-            <h1>Countries</h1>
-        </div>
-    )
+    
+    //Make a state to set data in √
+    //Set up a useEffect to control my components lifecycle √
+    //Organize API Links / URL √
+    //Make API call √
+    //Set our data in state and log it √
+    //render our data
+    //Set up guard
+    
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        const url = 'https://restcountries.com/v3.1/all'
+
+        const getCountries = async () => {
+            const response = await axios.get(url)
+            console.log(response.data)
+            setCountries(response.data)
+        }
+
+        getCountries()
+
+    },[])
+
+    if (!countries) {
+        return <h1> loading please wait</h1>
+        
+        
+    } else {
+        return (
+            <div>
+                <h1>Countries</h1>
+                {/* <h2>{countries[3].name.official}</h2> */}
+                
+                {countries ? countries.map((country, index) => (<div key={index}><h3>{country.name.official}</h3></div>    )):null }
+            </div>)
+
+        
+        
+    }
 }
