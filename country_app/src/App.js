65f2c16b1223af57d8b8dddd882f561ce6f43c2f
Austin <austinih46@gmail.com>
2023-02-24 10:22:42 -0600

    created react-app

---
+++ country_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React is working</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
