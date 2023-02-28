c858f861383b19319abcfa1eac5703d3651d4456
Austin <austinih46@gmail.com>
2023-02-28 15:32:40 -0600

    changed title for Lang Results

--- country_app/src/components/langComponents/LangRSLT.js
+++ country_app/src/components/langComponents/LangRSLT.js
@@ -37,7 +37,7 @@ export default function LangRSLT (props) {
     } else {
         return (
             <div className='countryContainer'>
-                <h1 className='listTitle' style={{textDecoration: 'underline', textDecorationColor: 'rgb(16, 220, 64)', textDecorationThickness: '5px'}}>All Countries </h1>
+                <h1 className='listTitle' style={{textDecoration: 'underline', textDecorationColor: 'rgb(16, 220, 64)', textDecorationThickness: '5px'}}>{countryInfo.spokenLanguage} Speaking Countries </h1>
                 
                 <div className='countryList'>
                     {props.countries ? props.countries.map((country, index) => (
