ef193d3b6d3fc09602071bcf7bb9d501669aa467
Austin <austinih46@gmail.com>
2023-03-02 15:19:55 -0600

    Country Details first Draft

--- country_app/src/components/CountryDetailsV2.jsx
+++ country_app/src/components/CountryDetailsV2.jsx
@@ -16,14 +16,36 @@ export default function CountryDetailsV2 (props) {
     },[props.countries, index])
     
     
-        return country && country.flags ? (
-            <div>
-                <img src={country.flags.png} alt='flag' className='flag'></img>
-                <h1 className='countryName'>{country.name.common}</h1>   
+        return country && country.flags && country.languages ? (
+            <div className='details_page'>
+                <div className='leftBox'></div>
+                <div className='image_container'>
+                    <img src={country.flags.png} alt='flag' className='flag'></img>
+                    <img src={country.coatOfArms.png} alt='Coat of Arms' className='coatOfArms' ></img>
+                </div>
+               
+
+                <div>
+                    <h1 className='countryName'>{country.name.common}</h1>   
+                    <div className='country_facts'>
+                        <h3>Official Name: {country.name.official}</h3>
+                        <h3>Capital: {country.capital}</h3>
+                        <h3>Population: {country.population}</h3>
+                        {/* <h3>Currencies: {country.currencies}</h3> */}
+                        <h3>Continent: {country.continents}</h3>
+                        <h3>Region: {country.region}</h3>
+                        <h3>Sub-Region: {country.subregion}</h3>
+                        {/* <h3>Languages: {country.languages}</h3> */}
+                        {/* <h3>UN Member: {country.unMember}</h3> */}
+                        <h3>Latitude & Longitude: {country.latlng}</h3>
+                        {/* <h3>Landlocked: {country.landlocked}</h3> */}
+                        <h3>Bordering Countries: {country.borders}</h3>
+                        <h3>Area in km²:{country.area}</h3>
+                        <h3>Driving side:{country.car.side}</h3> 
+                    </div>
+                </div>
+                <div className='rightBox'></div>
                 
-                <h3>Official Name: {country.name.official}</h3>
-                <h3>Capital: {country.capital}</h3>
-    
             </div>
         ) : <h1> Loading, please wait</h1>
     
