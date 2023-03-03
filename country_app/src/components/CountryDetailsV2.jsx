c082546807ab5e7d6dad39aac77e960cd2afc541
Austin <austinih46@gmail.com>
2023-03-02 18:41:27 -0600

    additional CSS on countryDetails page

--- country_app/src/components/CountryDetailsV2.jsx
+++ country_app/src/components/CountryDetailsV2.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import { useNavigate} from "react-router-dom";
 import DataContext from '../DataContext'
+import { JsonFunction } from 'react-router-dom';
 
 
 
@@ -21,32 +22,33 @@ export default function CountryDetailsV2 (props) {
         setCountry(selectedCountry)
     },[props.countries, index])
     
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     
         return country && country.flags && country.languages ? (
             <div className='details_page'>
                 <div className='leftBox' style={{backgroundColor:countryInfo.colorTheme}}></div>
-                <div className='image_container' style={{backgroundColor:countryInfo.colorTheme}}>
+                <div className='image_container' >
                     <img src={country.flags.png} alt='flag' className='flag'></img>
                     <img src={country.coatOfArms.png} alt='Coat of Arms' className='coatOfArms' ></img>
                 </div>
-               
-
                 <div>
                     <h1 className='countryName' style={{backgroundColor:countryInfo.colorTheme}}>{country.name.common}</h1>   
                     <div className='country_facts' style={{borderColor:countryInfo.colorTheme}}>
-                        <h3>Official Name: {country.name.official}</h3>
-                        <h3>Capital: {country.capital}</h3>
-                        <h3>Population: {country.population}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Official Name:</span> {country.name.official}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Capital:</span> {country.capital}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Population:</span> {country.population}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Continent:</span> {country.continents}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Region:</span> {country.region}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Sub-Region:</span> {country.subregion}</h3>
                         {/* <h3>Currencies: {country.currencies}</h3> */}
-                        <h3>Continent: {country.continents}</h3>
-                        <h3>Region: {country.region}</h3>
-                        <h3>Sub-Region: {country.subregion}</h3>
                         {/* <h3>Languages: {country.languages}</h3> */}
                         {/* <h3>UN Member: {country.unMember}</h3> */}
                         {/* <h3>Landlocked: {country.landlocked}</h3> */}
-                        <h3>Bordering Countries: {country.borders}</h3>
-                        <h3>Area in km²:{country.area}</h3>
-                        <h3>Driving side:{country.car.side}</h3> 
+                        {/* <h3><span style={{color:countryInfo.colorTheme}}>Bordering Countries:</span> {country.borders}.</h3> */}
+                        <h3><span style={{color:countryInfo.colorTheme}}>Area in km²:</span> {country.area}</h3>
+                        <h3><span style={{color:countryInfo.colorTheme}}>Driving side:</span> {country.car.side}</h3> 
                         <button onClick={() => navigate(-1)}>Back</button>
                     </div>
                 </div>
